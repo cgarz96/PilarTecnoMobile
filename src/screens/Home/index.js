dcf72b50e90a1fc7030e09e5bcbb01ed96d63ce5
cgarz96 <cgarzon388@gmail.com>
2022-03-14 23:08:14 -0300

    Agregando botones

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,7 +2,8 @@ import React, { Component, useCallback } from 'react';
 import { 
     SafeAreaView,
     View, 
-    Text
+    Text,
+    Pressable
 } from 'react-native';
 import { styles } from './styles'
 
@@ -10,9 +11,24 @@ export default Home = () => {
 
     return(
         <SafeAreaView style={styles.container}>
-            <Text>
-                Homecsss
-            </Text>
+            <View>
+               <View>
+                   <Pressable onPress={() => console.log('home')}>
+                       <Text>Home 1</Text>
+                   </Pressable>
+                   <Pressable>
+                       <Text>Home 2</Text>
+                   </Pressable>
+               </View>
+               <View>
+                   <Pressable>
+                       <Text>Home 3</Text>
+                   </Pressable>
+                   <Pressable>
+                       <Text>Home 4</Text>
+                   </Pressable>
+               </View>
+            </View>
         </SafeAreaView>
     )
 }
